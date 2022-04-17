f3408c88c14b98e480928ddced0bf50bbe34811f
Ekaterina <ekaterina_r21@mail.ru>
2022-04-17 21:18:35 +0300

    Задача2

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+int max = numberA;
+if (numberA > numberB) max = numberA;
+if (numberA < numberB) max = numberB;
+
+Console.WriteLine(max);
