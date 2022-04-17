f4861b96915f447272b9dc0d1401e4d85d445eae
Ekaterina <ekaterina_r21@mail.ru>
2022-04-17 23:06:04 +0300

    Задача 8

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите чиисло: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= N)
+{
+    if (i % 2 == 0) Console.Write(i + " ");
+    i++;
+}
+Console.WriteLine( );
