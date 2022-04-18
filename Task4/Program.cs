3819472dc99b93e6933c7297ffe5aa8f15d0ef73
Ekaterina <ekaterina_r21@mail.ru>
2022-04-18 12:41:18 +0300

    Задача 6

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите чиисло: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
